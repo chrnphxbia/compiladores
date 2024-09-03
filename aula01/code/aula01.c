a2128fa897cba0060fd833f88adadb2fc605e998
chrnphxbia <pedrohqfarias@gmail.com>
2024-09-03 15:02:55 -0300

    Iniciando repositório

---
+++ aula01/code/aula01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    FILE* file_pointer = fopen("file.txt", "r");
+
+    return 0;
+}
---
+++ aula01/code/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) { // no one has ever done it lol 
+    printf("Hello, world!\n");
+    return 0;
+}
